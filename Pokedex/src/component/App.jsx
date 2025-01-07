4ca3e6f5910107090221c4eac6b67acaacb138b2
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2025-01-07 15:00:27 +0530

    Setup the React App
    Created and setup the React files and Vite app

---
+++ Pokedex/src/component/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import '../styles/App.css'
+
+function App() {
+  return (
+    <div>
+      <div id="PokedexTitle">Pokedex</div>
+      <div id="search"></div>
+      <div id="display"></div>
+    </div>
+  )
+}
+
+export default App
